2c0e54102159b8cc04d6d04ddcd4ce5557331639
Seungmin Kang <autumnsky1562@gmail.com>
2026-03-05 13:12:20 +0900

    이슈 해결

--- src/Main.java
+++ src/Main.java
@@ -16,9 +16,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
+        // test가 너무 많이 출력되서 지움
         for (int i = 1; i <= 5; i++) {
             //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
             // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
