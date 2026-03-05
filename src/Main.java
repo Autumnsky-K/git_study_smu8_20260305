a481c6241d4c4598c011a716d75d451de61d80e6
Seungmin Kang <autumnsky1562@gmail.com>
2026-03-05 13:14:31 +0900

    성적 스위치 추가

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test");
         // test가 너무 많이 출력되서 지움
+        int score = 77;
+        String grade = switch (score/10) {
+
+            case 7 -> "c";
+
+            default -> throw new IllegalStateException("Unexpected value: " + score);
+        };
+
         for (int i = 1; i <= 5; i++) {
             //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
             // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
