c9a8b1f9ef7b984162128c1d5b009eeaa7cf83d0
Seungmin Kang <autumnsky1562@gmail.com>
2026-03-05 13:04:18 +0900

    sum 기능 추가

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
+
+    public void sum(int a, int b) {
+        System.out.printf("a+b=%d", a+b);
+    }
+
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test");
